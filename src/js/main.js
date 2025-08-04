254a9deb8c92133582a3b448c1d1289b18ca3e2b
vladimirovicp <vladimirovicp70@gmail.com>
2025-08-04 12:23:13 +0400

    feat: add progress start

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,55 @@
 // import "./lib/app";
 
 console.log('Привет!')
+
+
+// $(".progress span").each(function () {
+//   $(this).animate(
+//     {
+//       width: $(this).attr("data-progress") + "%",
+//     },
+//     1000
+//   );
+//   $(this).text($(this).attr("data-progress") + "%");
+// });
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Получаем все элементы span внутри элементов с классом progress
+  const progressSpans = document.querySelectorAll('.progress-bar');
+
+  // Перебираем каждый элемент span
+  progressSpans.forEach(function(span) {
+    // Получаем значение data-progress
+    // const progress = span.getAttribute('data-progress');
+
+    // // Устанавливаем ширину и текст
+    // span.style.width = progress + '%';
+    // //span.textContent = progress + '%';
+
+    // const parent = span.parentElement;
+    // const progressText = progress + '%';
+    //  parent.appendChild(progressText);
+
+
+    const progress = span.getAttribute('data-progress');
+
+    // Устанавливаем ширину для span
+    span.style.width = progress + '%';
+
+    // Создаем новый элемент для отображения текста
+    const progressText = document.createElement('div');
+    progressText.className = 'progress__number';
+    progressText.textContent = progress + '%';
+
+    // Добавляем текстовый элемент в родителя
+    const parent = span.parentElement;
+    parent.appendChild(progressText);
+
+
+  });
+});
+
+
+
