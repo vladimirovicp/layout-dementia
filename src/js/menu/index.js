3c6e3ed5fedf2fe9b86b75067d60cd1781d496ad
vladimirovicp <vladimirovicp70@gmail.com>
2025-08-15 12:15:47 +0400

    feat: add process header adaptive

--- src/js/menu/index.js
+++ src/js/menu/index.js
@@ -6,14 +6,17 @@ if(header){
     const nav = header.querySelector('.header__nav'); 
     if(nav){
         const menuBtn = nav.querySelector('.header__nav-wrapper');
-        menuBtn.addEventListener('click', () => menuClick(nav));
+        menuBtn.addEventListener('click', () => menuClick(nav,header));
     }
    
 }
 
-function menuClick(nav){
+function menuClick(nav, header){
     const navMenu = nav.querySelector('.header__nav-menu');
     const icoBurger = nav.querySelector('.header__nav-hamburger')
     navMenu.classList.toggle('_active');
     icoBurger.classList.toggle('_active');
+
+    const container = header.querySelector('.container');
+    container.classList.toggle('_menu-active');
 }
