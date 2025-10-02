eb2a8750cb24e4904a415b748c71e2d8030cdbca
vladimirovicp <vladimirovicp70@gmail.com>
2025-10-02 16:20:14 +0400

    feat: add v 1

--- src/js/modal/index.js
+++ src/js/modal/index.js
@@ -29,11 +29,36 @@ function modalOpen(modal,modalBg){
      modal.classList.add('active');
      body.classList.add('_lock');
 
+     
+
      document.onclick = function (e) {
         if(e.target.className == "modal__bg active"){
             modalBg.classList.remove('active');
             modal.classList.remove('active');
             body.classList.remove('_lock');
         }
+
+        if( e.target.className == "footer__modal-bg active"){
+            modalBg.classList.remove('active');
+            modal.classList.remove('active');
+            body.classList.remove('_lock');
+        }
     };
+}
+
+const footerModalBg = document.querySelector('.footer__modal-bg');
+
+if(footerModalBg){
+    const footerModal = footerModalBg.querySelector('.footer__modal');
+    if(footerModal){
+
+        const footerModalOpenBtn = document.querySelector('.footer__modal-open');
+
+        if(footerModalOpenBtn){
+            footerModalOpenBtn.addEventListener('click', () => modalOpen(footerModal,footerModalBg));
+        }
+
+        const modalCloseBtn = footerModal.querySelector('.modal__close');
+        modalCloseBtn.addEventListener('click', ()=> modalClose(footerModal,footerModalBg));
+    }
 }
